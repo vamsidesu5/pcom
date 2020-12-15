e19e08a5beed4c298eac082d4817216f73835a9c
vamsidesu5 <vamsidesu5@gmail.com>
2020-12-15 17:03:04 -0500

    imports

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 # Train a per-frame classifier on NTU-RGBD for RGB data (use VGG16, you can get it in pytorch)
 import torchvision.models as models
+import os
 for files in os.listdir("./coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
     print(files)
