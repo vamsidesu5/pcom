dbdef6b0c7f03430478e86b1291c966081a71209
Vamsi Desu <vdesu7@zatopek.cc.gatech.edu>
2021-02-10 17:32:05 -0500

    new transfer models

--- classifier.py
+++ classifier.py
@@ -1,5 +1,7 @@
 # Train a per-frame classifier on NTU-RGBD for RGB data (use VGG16, you can get it in pytorch)
 import torchvision.models as models
 import os
-for files in os.listdir("./coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
-    print(files)
+import torchvision
+# for files in os.listdir("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
+#     print(files)
+torchvision.io.read_video("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
