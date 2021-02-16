d621b2ab25a896cf3d056580cea669c41c783e4f
Vamsi Desu <vamsidesu5@gmail.com>
2021-02-15 19:38:38 -0500

    using videoReader

--- classifier.py
+++ classifier.py
@@ -1,7 +1,16 @@
 # Train a per-frame classifier on NTU-RGBD for RGB data (use VGG16, you can get it in pytorch)
+# Dataset Info: http://rose1.ntu.edu.sg/datasets/actionrecognition.asp
+# Imports 
 import torchvision.models as models
 import os
 import torchvision
+
+video_path = "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"
+reader = torchvision.io.VideoReader(video_path, "S014C002P025R001A017_rgb.avi")
+reader_md = reader.get_metadata()
+print(reader_md["video"]["fps"])
+
 # for files in os.listdir("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
 #     print(files)
-torchvision.io.read_video("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
+video = torchvision.io.read_video("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
+# print(video.vframes)
