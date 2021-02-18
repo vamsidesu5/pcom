3725a5254579f99c4d7305b6206f721225142813
Vamsi Desu <vamsidesu5@gmail.com>
2021-02-18 16:59:32 -0500

    use of torchvision

--- classifier.py
+++ classifier.py
@@ -6,11 +6,11 @@ import os
 import torchvision
 
 video_path = "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"
-reader = torchvision.io.VideoReader(video_path, "S014C002P025R001A017_rgb.avi")
-reader_md = reader.get_metadata()
-print(reader_md["video"]["fps"])
+# reader = torchvision.io.VideoReader(video_path, "S014C002P025R001A017_rgb.avi")
+# reader_md = reader.get_metadata()
+# print(reader_md["video"]["fps"])
 
 # for files in os.listdir("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
 #     print(files)
 video = torchvision.io.read_video("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
-# print(video.vframes)
+print(video[0].shape)
