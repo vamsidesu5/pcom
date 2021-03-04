d1d4f67743e883c0e90cad36fb83ba62d512ff97
Vamsi Desu <vamsidesu5@gmail.com>
2021-03-03 19:32:18 -0500

    added dictionary to keep track of videos and action classes for each

--- classifier.py
+++ classifier.py
@@ -7,18 +7,27 @@ import torchvision.models as models
 import os
 import torchvision
 
-video_path = "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"
-vgg16 = models.vgg16()
-# reader = torchvision.io.VideoReader(video_path, "S014C002P025R001A017_rgb.avi")
-# reader_md = reader.get_metadata()
-# print(reader_md["video"]["fps"])
+video_path = "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb" #VIDEO_PATH: path to all RGB videos for NTU-RGBD dataset
+vgg16 = models.vgg16() # vgg16: VGG16 model (type of CNN)
+videos = {} # VIDEOS: dictionary of label to list of video names
+count = 0 # COUNT: total number of videos being inputted as test or train data 
+# Reading through all files in VIDEO_PATH and putting all videos in the matching action label's list in dictionary
 
-count = 0
 for files in os.listdir("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
-    if int(files[17:20]) <= 20 & int(files[17:20]) >= 1:
+    if int(files[17:20]) <= 20 and int(files[17:20]) >= 1:
         print(files)
+        print(int(files[17:20]))
+        if int(files[17:20]) in set(videos.keys()):
+            videos[int(files[17:20])].append(files)
+        else:
+            videos[int(files[17:20])] = [files]
         count += 1
+print(len(videos.get(11)))
 print(count)
-video = torchvision.io.read_video(
-    "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
-print(video[0].shape)
+
+
+
+
+# video = torchvision.io.read_video(
+#     "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
+# print(video[0].shape)
