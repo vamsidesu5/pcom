27ee42619af9ef925d24efbb329f045a9ef838cc
Vamsi Desu <vamsidesu5@gmail.com>
2021-04-08 15:41:10 -0400

    file for image net model

--- classifier.py
+++ classifier.py
@@ -9,27 +9,52 @@ import torchvision
 
 # VIDEO_PATH: path to all RGB videos for NTU-RGBD dataset
 video_path = "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"
-vgg16 = models.vgg16()  # vgg16: VGG16 model (type of CNN)
-videos = {}  # VIDEOS: dictionary of label to list of video names
+train_videos = {}  # train_videos: dictionary of label to list of video names for training
+test_videos = {}  # test_videos: dictionary of label to list of video names for testing
 count = 0  # COUNT: total number of videos being inputted as test or train data
 # Reading through all files in VIDEO_PATH and putting all videos in the matching action label's list in dictionary
 
-subjectSet = set()
-for files in os.listdir("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
-    if int(files[17:20]) <= 120 and int(files[17:20]) >= 1:
-        print(files)
-        print(int(files[17:20]))
-        print("Subject ID" + files[0:4])
-        subjectSet.add(int(files[1:4]))
-        if int(files[17:20]) in set(videos.keys()):
-            videos[int(files[17:20])].append(files)
-        else:
-            videos[int(files[17:20])] = [files]
-        count += 1
-print(len(videos.get(100)))
-print(count)
-print(subjectSet)
+# torchvision.io.read_video("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")) 
 
-# video = torchvision.io.read_video(
-#     "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
-# print(video[0].shape)
+
+# def vgg16Model():
+#     vgg16 = models.vgg16(inputs=torchvision.io.read_video(
+#         "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi"))  # vgg16: VGG16 model (type of CNN)
+#     # model_VGG16 = VGG16(include_top = False, weights = None)
+#     # model_input = Input(shape = image_shape, name = 'input_layer')
+#     # output_VGG16_conv = model_VGG16(model_input)
+#     # #Init of FC layers
+#     # x = Flatten(name='flatten')(output_VGG16_conv)
+#     # x = Dense(256, activation = 'relu', name = 'fc1')(x)
+#     # output_layer = Dense(num_classes,activation='softmax',name='output_layer')(x)
+#     # vgg16 = Model(inputs = model_input, outputs = output_layer)
+#     # vgg16.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+#     vgg16.summary()
+#     return vgg16
+
+
+# vgg16Model()
+
+# subjectSet = set()
+# for files in os.listdir("/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb"):
+#     if int(files[17:20]) <= 120 and int(files[17:20]) >= 1:
+#         print(files)
+#         print(int(files[17:20]))
+#         print("Subject ID" + files[0:4])
+#         subjectSet.add(int(files[1:4]))
+#         if int(files[17:20]) in set(train_videos.keys()) and int(files[1:4]) <= 16:
+#             train_videos[int(files[17:20])].append(files)
+#         elif int(files[17:20]) not in set(train_videos.keys()) and int(files[1:4]) <= 16:
+#             train_videos[int(files[17:20])] = [files]
+#         elif int(files[17:20]) in set(test_videos.keys()) and int(files[1:4]) > 16:
+#             test_videos[int(files[17:20])].append(files)
+#         elif int(files[17:20]) not in set(test_videos.keys()) and int(files[1:4]) > 16:
+#             test_videos[int(files[17:20])] = [files]
+#         count += 1
+# print(test_videos)
+# print(subjectSet)
+
+
+video = torchvision.io.read_video(
+    "/coc/pcba1/Datasets/public/NTU_RGBD/nturgb+d_rgb/S014C002P025R001A017_rgb.avi")
+print(video[0].shape)
