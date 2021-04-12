cacb82f48a7a920d842fec8b4f0021d0c8c09940
Vamsi Desu <vamsidesu5@gmail.com>
2021-04-12 18:48:52 -0400

    added extra images to test alexnet on

--- imagenet_modeling/model.py
+++ imagenet_modeling/model.py
@@ -14,7 +14,7 @@ transform = transforms.Compose([            #[1] Defining a transforrm which is 
  mean=[0.485, 0.456, 0.406],                
  std=[0.229, 0.224, 0.225]                  
  )])
-img = Image.open("dog.jpg")
+img = Image.open("shovel.jpg")
 img_t = transform(img)
 # print(img_t.shape) # shape: [3, 224, 224]
 batch_t = torch.unsqueeze(img_t, 0) # Returns a new tensor with a dimension of size one inserted at the specified position. 
@@ -28,6 +28,8 @@ val, index = torch.max(out, 1) # Returns the maximum value of all elements in th
 # print(index)
 percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100 #calculating percentage of each class possibility using softmax
 # print(percentage[index[0]])
-print(classes[index[0]], percentage[index[0]].item())
+# print(classes[index[0]], percentage[index[0]].item())
 _, indices = torch.sort(out, descending=True) # sorts all percentages in descending order 
 [print(classes[idx], percentage[idx].item()) for idx in indices[0][:5]] # top 5 classes predicted 
+
+# https://learnopencv.com/pytorch-for-beginners-image-classification-using-pre-trained-models/
